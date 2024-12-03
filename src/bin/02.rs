276d1b8323ca9440e38a32e5d8d5e5aa219bfbb2
George <george@fozzie.dev>
2024-12-03 23:00:01 +0000

    Fix clippy

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -38,7 +38,7 @@ pub fn part_one(input: &str) -> Option<i32> {
     Some(safe_total)
 }
 
-pub fn part_two(input: &str) -> Option<i32> {
+pub fn part_two(_input: &str) -> Option<i32> {
     None
 }
 
